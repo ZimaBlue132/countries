cd50983787710257e7c61f9d489fed3187caeef9
Carl.Frenke <carl.frenke@ladmins-MBP.fritz.box>
2020-07-20 01:12:57 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import axios from 'axios'
+import Country from './Components/Country'
 
 axios.get('https://restcountries.eu/rest/v2/all').then(response => {
   const countries = response.data
   ReactDOM.render(
     <App countries={countries} />,
+    <Country countries={countries}/>,
     document.getElementById('root')
   )
 })
